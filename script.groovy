898d728c545dfb0d2ec6db70e04955f0d626730d
Timur Atabaev <38188133+atabaeph@users.noreply.github.com>
2021-07-02 15:12:00 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildWar() {
 def buildDockerImg() {
 	echo 'building docker image....'
 	withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'DOCKER_ID')]) {
-		sh 'docker build -t abubandit/demo-app:1.0 .'
+		sh 'docker build -t abubandit/demo-app:1.1 .'
 		sh 'echo $PASS | docker login -u $DOCKER_ID --password-stdin'
-		sh 'docker push abubandit/demo-app:1.0'
+		sh 'docker push abubandit/demo-app:1.1'
 	}
 }
 
